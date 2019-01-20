ce57bc559aae21acaec92c8ed57bce89a72e0a8d
tjtanaa <tunjian1996@gmail.com>
2019-01-20 16:48:45 +0800

    update vott_azure functions

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Microsft x DJI: Drone Competition
 
-This repository stores the training/testing datasets (originally from [here](https://github.com/Horea94/Fruit-Images-Dataset)) altered for the captioned competition. 
+This repository stores the training/testing datasets (originally from [here](https://github.com/Horea94/Fruit-Images-Dataset)) altered for the captioned competition.
+
+The models are trained using the dataset captured and tagged by all the collaborators. The dataset can be found [here](https://www.kaggle.com/tjtanaa/yolo-fruits-dataset-for-dji-wsdk-object-detection). 
--- azure_to_vott/azure_to_vott.py
+++ azure_to_vott/azure_to_vott.py
@@ -2,15 +2,13 @@ import os
 import json
 from pprint import pprint
 from sklearn.model_selection import train_test_split
+import argparse
 
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 
 list_of_image_name = []
 
-# to avoid name conflit an offset is added
-
-
-def azure_to_vott():
+def azure_to_vott(test_size):
     with open(os.path.join(curr_dir,'obj/anno.json')) as f:
         data = json.load(f)
 
@@ -34,34 +32,11 @@ def azure_to_vott():
         for t in train:
             f.write(t + "\n")
 
-def vott_to_azure():
-    json_struct = {}
-    for root, dirs, files in os.walk(curr_dir+"/obj"):
-        for file in files:
-            if file.endswith(".json"):
-                print(os.path.join(root, file))
-                data_list = []
-                with open(os.path.join(root, file), "r") as f_in:
-                    line = f_in.readline().split(" ")
-                    while line:
-                        data = {}
-                        data["id"] = line[0]
-                        x = float(line[1])
-                        y = float(line[2])
-                        w = float(line[3])
-                        h = float(line[4])
-                        x = x - w / 2.
-                        y = y - h / 2.
-                        data["bbox"] = [x, y, w, h]
-                        data_list.append(data)
-                        line = f_in.readline()
-                json_struct[file.replace(".txt","")] = data_list
-    print(json_struct)
-    with open(curr_dir+'/vott_tagged_file/data/obj/anno.json', 'w+') as outfile:  
-        json.dump(json_struct, outfile)
-
 if __name__ == '__main__':
-    # print(curr_dir)
-    azure_to_vott()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-test_size", default=None, required=True,
+                        help="Please provide the portion of test dataset in range 0 to 1")
+    opt = parser.parse_args()
+    azure_to_vott(opt.test_size)
 
 
